6bf78ad5ac4f7263190fe82d11e46e46c54b5f63
deeksha957 <deeksha8758@gmail.com>
2021-10-23 15:58:40 +0530

    commit

---
+++ threenumb.js
@@ -0,0 +1,16 @@
+var x=908;
+var y=708;
+var z=112;
+if((x<y)&&(x<z))
+{
+    console.log(x,"lesser than",y,z);
+}
+else if((y<x)&&(y<z))
+{
+    console.log(y,"lesser than",x,z);
+
+}
+else if((z<x)&&(z<y))
+{
+    console.log(z,"lesser than",x,y);
+}
