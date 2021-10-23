cf0e4babaca2618e77e48d0e6a36071dce6b4dc4
deeksha957 <deeksha8758@gmail.com>
2021-10-23 15:54:09 +0530

    https://github.com/DEEKSHA957/Full-stack--Pract

---
+++ lesser.js
@@ -0,0 +1,10 @@
+var x=233;
+var y=57;
+if(x<y)
+{
+    console.log("lesser is",x);
+}
+else
+{
+    console.log("lesser is",y);
+}
---
+++ threenum.js
@@ -0,0 +1,16 @@
+var x=9098;
+var y=708;
+var z=112;
+if((x>y)&&(x>z))
+{
+    console.log(x,"greater than",y,z);
+}
+else if((y>x)&&(y>z))
+{
+    console.log(y,"greater than",x,z);
+
+}
+else if((z>x)&&(z>y))
+{
+    console.log(z,"greater than",x,y);
+}
