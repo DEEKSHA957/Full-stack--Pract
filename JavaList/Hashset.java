1cf5c28d5617348ffe9f791219508919c488666b
DEEKSHA957 <92008362+DEEKSHA957@users.noreply.github.com>
2021-12-24 17:17:06 +0530

    commit

---
+++ JavaList/Hashset.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Hashset {
+public static void main(String[] args)
+
+    {
+     LinkedHashSet<String> hs =new LinkedHashSet<String>();
+      hs.add("HELLO!!!!");
+      hs.add("Hiii !!!!");
+         hs.add("How are you ????/");
+     System.out.println("LinkedHashSet : " + hs);
+    }
+}
---
+++ JavaList/Hashset2.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+
+ class Hashset2 {
+  public static void main(String[] args) {
+    HashSet<String> cars = new HashSet<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("BMW");
+    cars.add("Mazda");
+    System.out.println(cars);
+    cars.remove("BMW");
+    cars.remove("Mazda");
+    System.out.println(cars);
+  }
+}
