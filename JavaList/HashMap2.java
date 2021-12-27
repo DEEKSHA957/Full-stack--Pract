ef6695f0bbe19d44df1fc3445d21cf358e053979
DEEKSHA957 <92008362+DEEKSHA957@users.noreply.github.com>
2021-12-27 18:10:01 +0530

    commit

---
+++ JavaList/HashMap2.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+ class HashMapExample2 {
+public static void main(String[] args) {
+HashMap<String, Integer> ms = new HashMap<String, Integer>();
+ms.put("S", 35);
+ms.put("M", 38);
+ms.put("L", 40);
+ms.put("XL", 42);
+for (String key : ms.keySet()) {
+System.out.println("measurement of " + key + " in inch is: " + ms.get(key));
+}
+}
+}
