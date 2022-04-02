1b5de8ee036ce99d24da86b3aef70b92abfbe918
DEEKSHA957 <92008362+DEEKSHA957@users.noreply.github.com>
2022-04-02 22:36:25 +0530

    commit

---
+++ EH/NullPointer.java
@@ -0,0 +1,11 @@
+
+public class NullPointer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 String str = null;
+	        System.out.println(str.length());
+	         
+	}
+
+}
